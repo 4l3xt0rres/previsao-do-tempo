59d3c1fcb88b9eae6684d1a4e29fceaf1ab3729a
Alex Torres <s0ual3xsandro@gmail.com>
2025-09-17 15:50:18 -0300

    adicionando main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+cidade = str(input('digite a cidade que você deseja ver a previsão do tempo em até 7 dias:\n(se a cidade tiver 2 ou mais palavras use um - ao inves de um espaço por favor):')).lower()
+URL = f"https://www.timeanddate.com/weather/brazil/{cidade}/ext"
+
+def coletar_previsao():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")  
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(URL)
+
+    wait = WebDriverWait(driver, 10)
+    tabela = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.zebra.tb-wt.tb-hover")))
+
+    linhas = tabela.find_elements(By.TAG_NAME, "tr")
+    previsao = []
+
+    for linha in linhas[1:8]:  
+        try:
+            dia = linha.find_element(By.CSS_SELECTOR, "th").text.strip()
+            
+            if dia.lower() == "day" or dia == "":
+                continue
+        except:
+            continue  
+
+        try:
+            temp_max_and_min = linha.find_element(By.CSS_SELECTOR, "td:nth-child(3)").text.strip()
+        except:
+            temp_max_and_min = "N/A"
+
+        try:
+            condicao = linha.find_element(By.CSS_SELECTOR, "td:nth-child(4)").text.strip()
+        except:
+            condicao = "N/A"
+
+        previsao.append([cidade, dia, temp_max_and_min, condicao])
+
+    driver.quit()
+    return previsao
+
+def salvar_csv(previsao):
+    df = pd.DataFrame(previsao, columns=["Cidade","Dia", "Temperatura Máx e Min", "Condição"])
+    df.to_csv("previsao.csv", index=False, encoding="utf-8")
+    print("✅ Dados salvos em previsao.csv")
+
+if __name__ == "__main__":
+    dados = coletar_previsao()
+    salvar_csv(dados)
